ce1389ea463d49bcee345d0feb011c11b2a12d0d
DDN53 <dinukanisheda9@gmail.com>
2026-02-08 20:26:59 +0530

    modify content

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,10 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'   // if using React; adjust for your framework
+import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from '@cloudflare/vite-plugin'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-    cloudflare({
-      // optional config
-    }),
-  ],
-  build: {
-    rollupOptions: {
-      external: ['node:events', 'node:perf_hooks', 'node:tty', 'tailwindcss']
-    }
-  }
+    tailwindcss()
+  ]
 })
